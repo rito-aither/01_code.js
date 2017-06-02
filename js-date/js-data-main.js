62a85860c0dc3242ff688f69a345fd00da5f390b
rito-aither <lion.aither@gmail.com>
2017-06-02 15:24:21 +0900

    標準入力

--- js-date/js-data-main.js
+++ js-date/js-data-main.js
@@ -1,2 +1,11 @@
-var i , j ;
+process.stdin.resume();
+process.stdin.setEncoding(‘utf8’);
 
+process.stdin.on(‘data’, function(sentens) {
+var line = sentens.toString().split(“\n”);
+line.pop();
+line = line.toString().split(“,”);
+
+console.log(line);
+
+});
