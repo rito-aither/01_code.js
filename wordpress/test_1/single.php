0a2b3b01db4bb1d79ec934832ca5448a260005c9
rito-aither <lion.aither@gmail.com>
2018-05-25 16:24:53 +0900

    20180525 wordpresstraining

--- wordpress/test_1/single.php
+++ wordpress/test_1/single.php
@@ -1,6 +1,5 @@
 
 <?php get_header(  );?>
-<?php get_sidebar(); ?>
 
 <div>
 <?php if(have_posts()): while(have_posts()): the_post(); ?> <!-- ループ開始 -->
@@ -8,7 +7,8 @@
 <?php echo get_the_date(); ?> <!-- 投稿日時を表示 -->
 <?php the_category(', '); ?> <!-- カテゴリーを表示 -->
 <?php the_title(); ?> <!-- 記事タイトルを表示 -->
-<?php the_content(続きを読む); ?> <!-- 記事本文を表示 -->
+<?php the_post_thumbnail();?>
+<?php the_content(); ?> <!-- 記事本文を表示 -->
 
 <?php endwhile; endif; ?> <!-- ループ終了 -->
 </div>
