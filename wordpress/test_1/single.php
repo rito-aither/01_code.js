cf2d2a92ba1bac3c41116fafef91b29f8362a688
rito-aither <lion.aither@gmail.com>
2018-05-26 17:47:16 +0900

    2018/05/26wordpress

--- wordpress/test_1/single.php
+++ wordpress/test_1/single.php
@@ -6,7 +6,11 @@
 
 <?php echo get_the_date(); ?> <!-- 投稿日時を表示 -->
 <?php the_category(', '); ?> <!-- カテゴリーを表示 -->
+
+<div class = title >
 <?php the_title(); ?> <!-- 記事タイトルを表示 -->
+</div>
+
 <?php the_post_thumbnail();?>
 <?php the_content(); ?> <!-- 記事本文を表示 -->
 
